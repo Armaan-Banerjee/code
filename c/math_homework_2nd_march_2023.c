d9edb576bc2c54521b9ed76e0588beea86386976
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-02 23:14:15 +0000

    stuff

--- c/math_homework_2nd_march_2023.c
+++ c/math_homework_2nd_march_2023.c
@@ -3,6 +3,19 @@
 int seq(int num){
     int square = num * num;
     int variable_remover = 8 * num;
-    return square - variable_remover - 5
+    return square - variable_remover - 5;
 }
 
+int main(){
+    while (1) {
+        int i = 1;
+        int ans = seq(i);
+        if( ans > 0 ) {
+            printf("%d", , ii );
+            break;
+        }
+        else {
+            i++;
+        }
+    }
+}

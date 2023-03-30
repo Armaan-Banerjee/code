b5b29e7916b8c02374b91de6e72efada74e7c776
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-30 09:57:28 +0100

    stuff

--- c/learn/unions.c
+++ c/learn/unions.c
@@ -1,2 +1,15 @@
 #include<stdio.h>
-#include<strings.h>
+#include<string.h>
+
+union Data{
+    int i;
+    float f;
+    char str[20];
+}
+
+int main(){
+    union Data data;
+    printf( "Memory size occupied by data : %d\n", sizeof(data));
+
+    return 0;
+}

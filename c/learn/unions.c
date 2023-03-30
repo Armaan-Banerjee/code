e901a120d4c83328fc947047344957ba4c550597
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-30 10:00:34 +0100

    stuff

--- c/learn/unions.c
+++ c/learn/unions.c
@@ -11,7 +11,7 @@ int main( ) {
 
    union Data data;        
 
-   printf( "Memory size occupied by data : %d\n", sizeof(data));
+   printf( "Memory size occupied by data : %ld\n", sizeof(data));
 
    return 0;
 }

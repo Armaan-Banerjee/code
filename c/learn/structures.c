89c8f8a16835b5a01b9ebb68d3e43e5ae622e9d9
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-30 10:18:18 +0100

    stuff

--- c/learn/structures.c
+++ c/learn/structures.c
@@ -1,9 +1,27 @@
 #include<stdio.h>
 #include<string.h>
 
-struct Computer{
+struct Computers{
     char name[20];
     char CPU[20]
     char GPU[20]
     int RAM;
 };
+
+int main(){
+    struct Computers Dell;
+    struct Computers Lenovo;
+
+    strcpy(Dell.name, "XPS 15");
+    strcpy(Dell.CPU, "Intel");
+    strcpu(Dell.GPU, "N3060");
+    Dell.RAM = 8000;
+
+    strcpy(Lenovo.name, "Yoga");
+    strcpy(Lenovo.CPU, "AMD");
+    strcpy(Lenovo.GPU, "Integrated");
+    Lenovo.RAM = 4000;
+
+    
+
+}

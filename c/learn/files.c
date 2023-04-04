da5bade32017219ee512a96990e1fcceaa27f8b4
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-04-04 22:09:04 +0100

    stuff

--- c/learn/files.c
+++ c/learn/files.c
@@ -4,4 +4,9 @@
 int main(){
 
     FILE *fp;
+
+    fp = fopen("./hi.txt", "w+");
+    fprintf(fp, "This is testing for fprintf...\n");
+   fputs("This is testing for fputs...\n", fp);
+   fclose(fp);
 }

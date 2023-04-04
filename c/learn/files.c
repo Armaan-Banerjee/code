ee4cc143352b8f4ff4263aff288e409772b9586e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-04-04 22:11:02 +0100

    stuff

--- c/learn/files.c
+++ c/learn/files.c
@@ -11,4 +11,14 @@ int main(){
     fclose(fp);
     
     char buff[255];
+    fp = fopen("./hi.txt", "r");
+    fscanf(fp, "%s", buff);
+    printf("1 : %s\n", buff );
+
+    fgets(buff, 255, (FILE*)fp);
+    printf("2: %s\n", buff );
+
+    fgets(buff, 255, (FILE*)fp);
+    printf("3: %s\n", buff );
+    fclose(fp);
 }

8d0faee80227753367db2b92580fe10a072162bb
Armaan-Banerjee <banerjee.armaan@gmail.com>
2023-07-12 10:45:22 +0100

    starting wrok on articles thing just as a test

---
+++ go/web/frameworks/gin/better_test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
+
+
+var router *gin.Engine
+
+func main() {
+    
+    router := gin.Default()
+
+    router.LoadHTMLGlob("templates/*")
+
+    router.GET("/", showIndexPage)
+
+    router.Run(":8432")
+}

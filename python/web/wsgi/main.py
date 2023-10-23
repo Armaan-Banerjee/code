6b952ed8584b8cda9908caabe2b08083492da8f4
Armaan-Banerjee <banerjee.armaan@gmail.com>
2023-10-23 21:15:58 +0100

    more wsgi stuff

--- python/web/wsgi/main.py
+++ python/web/wsgi/main.py
@@ -7,11 +7,32 @@ with socket.socket() as s:
 
     while True:
         with conn:
-            request = conn.recv(1024).decode('utf-8')
-            print(request)
-            parse_http(request)
+            http_request = conn.recv(1024).decode('utf-8')
+            request = parse_http(request)
+            response = view(request)
+            http_respnse = process_response(response)
             conn.sendall('hello world'.encode('utf-8'))
 
 
-def parse_http(request):
-    *headers = request
+def parse_http(http):
+    request, *headers, _, body = http.split('\r\n')
+    method, path, protocl = request.split(' ')
+    headers = dict(
+        line.split(':', maxsplit=1)
+        for line in headers
+    )
+
+    return method, path, protocl, headers, body
+
+def process_response(response):
+    return (
+        'HTTP/1.1 200 OK\r\n'
+        f'Content-Length: {len(response)}\r\n'
+        'Content-Type: text/html\r\n'
+        '\r\n'
+        response
+        '\r\n'
+    )
+
+def view(request):
+    return request
